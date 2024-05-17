ca8c22d09e16638d58831d095c464793b3c47d61
synapticloop <contact@synapticloop.com>
2024-05-17 17:29:06 +1000

    added in generated example files

--- src/main/java/com/synapticloop/panl/generator/PanlGenerator.java
+++ src/main/java/com/synapticloop/panl/generator/PanlGenerator.java
@@ -67,7 +67,7 @@ public class PanlGenerator {
 		// time to go through them and generate the panl.properties file
 
 		generatePanlDotProperties();
-		for(Collection collection: collections) {
+		for (Collection collection : collections) {
 			generateCollectionDotPanlDotProperties(collection);
 		}
 
@@ -83,7 +83,7 @@ public class PanlGenerator {
 
 			String line;
 			while ((line = reader.readLine()) != null) {
-				if(line.startsWith("$")) {
+				if (line.startsWith("$")) {
 					System.out.println(collection.getPanlProperty(line));
 				} else {
 					System.out.println(line);
@@ -96,19 +96,23 @@ public class PanlGenerator {
 
 	private void generatePanlDotProperties() {
 		StringBuilder sb = new StringBuilder();
-		for(Collection collection: collections) {
-			sb.append(collection.getCollectionName().toLowerCase().replaceAll("[^a-z0-9]", "-"))
+		for (Collection collection : collections) {
+			String niceCollectionName = collection.getCollectionName().toLowerCase().replaceAll("[^a-z0-9]", "-");
+			sb.append("panl.collection.")
+					.append(niceCollectionName)
+					.append("=")
+					.append(niceCollectionName)
 					.append(".panl.properties\n");
 		}
 
 		try (
 				InputStream inputStream = PanlGenerator.class.getResourceAsStream("/panl.template.properties");
-		     InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-		     BufferedReader reader = new BufferedReader(streamReader)) {
+				InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+				BufferedReader reader = new BufferedReader(streamReader)) {
 
 			String line;
 			while ((line = reader.readLine()) != null) {
-				if(line.startsWith("$panl.collection")) {
+				if (line.startsWith("$panl.collection")) {
 					System.out.println(sb.toString());
 				} else {
 					System.out.println(line);
