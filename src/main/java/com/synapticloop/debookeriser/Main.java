1d0ee7c87339c3db12e0122c024ca0c43c366aec
synapticloop <contact@synapticloop.com>
2024-06-22 17:28:34 +1000

    start of conversion

---
+++ src/main/java/com/synapticloop/debookeriser/Main.java
@@ -0,0 +1,59 @@
+package com.synapticloop.debookeriser;
+
+/*
+ * Copyright (c) 2008-2024 synapticloop.
+ *
+ * https://github.com/synapticloop/panl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	private final File googleDocsHTMLFile;
+	public Main(String fileName) {
+		this.googleDocsHTMLFile = new File(fileName);
+	}
+
+	public void generate() {
+		parseGoogleHTMLFile();
+	}
+	public void parseGoogleHTMLFile() {
+
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main(args[0]);
+		main.generate();
+	}
+}
