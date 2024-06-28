33b03c479858d6a1909dabbad49df9e6763c6660
Synapticloop <synapticloop@users.noreply.github.com>
2024-06-29 07:29:55 +1000

    integration testing

---
+++ src/integration/java/json/Panl.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008-2024 synapticloop.
+ *
+ * https://github.com/synapticloop/panl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+package json;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import json.panl.Available;
+import json.panl.Pagination;
+import json.panl.QueryOperand;
+import json.panl.Sorting;
+import json.panl.timings.Timings;
+
+import java.util.Map;
+
+public class Panl {
+	public Timings timings;
+	public Pagination pagination;
+	public QueryOperand query_operand;
+	public Sorting sorting;
+	public String canonical_uri;
+	public String query_respond_to;
+
+	public Available available;
+	public Map<String, String> fields;
+
+	@JsonIgnore public Object active;
+}
