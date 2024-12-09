c4c9c951abacc8d292ac7e6a8a46006cb36ae305
synapticloop <contact@synapticloop.com>
2024-12-09 12:47:03 +1100

    Added in solr.numrows.maximum properties to sample files, updated test files

--- src/test/java/com/synapticloop/panl/server/handler/helper/PropertyHelperTest.java
+++ src/test/java/com/synapticloop/panl/server/handler/helper/PropertyHelperTest.java
@@ -1,26 +1,30 @@
 package com.synapticloop.panl.server.handler.helper;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Properties;
 
 public class PropertyHelperTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(PropertyHelperTest.class);
 	@Test public void testDefault() {
 		PropertyHelper propertyHelper = new PropertyHelper();
 
 		Properties properties = new Properties();
 		int defaultValue = 100;
-		assertEquals(defaultValue, PropertyHelper.getIntProperty(properties, "missing.key", defaultValue));
+		assertEquals(defaultValue, PropertyHelper.getIntProperty(LOGGER, properties, "missing.key", defaultValue));
 
 		properties.put("key", "100");
-		assertEquals(defaultValue, PropertyHelper.getIntProperty(properties, "key", defaultValue));
+		assertEquals(defaultValue, PropertyHelper.getIntProperty(LOGGER, properties, "key", defaultValue));
 
 		properties.put("key", "askdkjhsadfkjh");
-		assertEquals(defaultValue, PropertyHelper.getIntProperty(properties, "key", defaultValue));
+		assertEquals(defaultValue, PropertyHelper.getIntProperty(LOGGER, properties, "key", defaultValue));
 
 		properties.put("key", "100L");
-		assertEquals(defaultValue, PropertyHelper.getIntProperty(properties, "key", defaultValue));
+		assertEquals(defaultValue, PropertyHelper.getIntProperty(LOGGER, properties, "key", defaultValue));
 
 	}
 }
