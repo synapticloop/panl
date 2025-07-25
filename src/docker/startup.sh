1ea5b0acc84e412288aeea978c0dfd0b87026949
synapticloop <contact@synapticloop.com>
2025-07-25 17:34:38 +1000

    Added in Dockerfile and associated files for building

---
+++ src/docker/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /java-servers/solr/ && bin/solr start --force --user-managed
+cd /java-servers/solr/ && bin/solr create -c mechanical-pencils -d mechanical-pencils
+cd /java-servers/solr/ && bin/solr post -c mechanical-pencils /java-servers/panl/sample/data/mechanical-pencils.json
+cd /java-servers/panl/ && bin/panl server -properties /java-servers/panl/sample/panl/mechanical-pencils/panl.properties &
+
+while true; do sleep 10000; done
