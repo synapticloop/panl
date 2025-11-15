1fff8e7ce5cbc2876bf6e0193eac1e46cd7efaf2
synapticloop <contact@synapticloop.com>
2025-11-15 11:36:46 +1100

    Fixed List of SolrJ implementations that are supported

--- src/main/java/com/synapticloop/panl/server/client/impl/PanlCloudHttp2SolrClient.java
+++ src/main/java/com/synapticloop/panl/server/client/impl/PanlCloudHttp2SolrClient.java
@@ -1,4 +1,4 @@
-package com.synapticloop.panl.server.client;
+package com.synapticloop.panl.server.client.impl;
 
 /*
  * Copyright (c) 2008-2025 synapticloop.
@@ -24,23 +24,47 @@ package com.synapticloop.panl.server.client;
  * IN THE SOFTWARE.
  */
 
+/*
+ * Copyright (c) 2008-2024 synapticloop.
+ *
+ * https://github.com/synapticloop/panl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 import com.synapticloop.panl.exception.PanlServerException;
-import com.synapticloop.panl.server.handler.properties.PanlProperties;
+import com.synapticloop.panl.server.client.PanlClient;
 import com.synapticloop.panl.server.handler.properties.CollectionProperties;
+import com.synapticloop.panl.server.handler.properties.PanlProperties;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudHttp2SolrClient;
-import org.apache.solr.client.solrj.impl.CloudSolrClient;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class PanlCloudSolrClient extends PanlClient {
+public class PanlCloudHttp2SolrClient extends PanlClient {
 	public static final String PREFIX_ZOOKEEPER = "zookeeper:";
 	private final List<String> solrUrls = new ArrayList<>();
 	private boolean hasZookeeper = false;
 
-	public PanlCloudSolrClient(String solrCollection, PanlProperties panlProperties, CollectionProperties collectionProperties) throws PanlServerException {
+	public PanlCloudHttp2SolrClient(String solrCollection, PanlProperties panlProperties, CollectionProperties collectionProperties) throws PanlServerException {
 		super(solrCollection, panlProperties, collectionProperties);
 
 		String[] urls = panlProperties.getSolrSearchServerUrl().split(",");
@@ -57,9 +81,9 @@ public class PanlCloudSolrClient extends PanlClient {
 	@Override
 	public SolrClient getClient() {
 		if (hasZookeeper) {
-			return (new CloudSolrClient.Builder(solrUrls, Optional.empty()).build());
+			return (new CloudHttp2SolrClient.Builder(solrUrls, Optional.empty()).build());
 		} else {
-			return (new CloudSolrClient.Builder(solrUrls).build());
+			return (new CloudHttp2SolrClient.Builder(solrUrls).build());
 		}
 	}
 }
